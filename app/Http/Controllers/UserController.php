3bd852f8f24660f275264cf7f8eb43c96107feb6
Nova Eliza Maharani <novaelzrayya@gmail.com>
2025-03-03 12:52:18 +0700

    pratikum 2.3 - jobsheet 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index() {
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        $user = UserModel::where('level_id', 2)->count();
         return view('user', ['data' => $user]); 
     }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -7,17 +7,11 @@
         <h1>Data User</h1>
         <table border="1" cellpadding="2" cellspacing="0">
             <tr>
-                <th>ID</th>
-                <th>Username</th>
-                <th>Nama</th>
-                <th>ID Level Pengguna</th>
+                <th>Jumlah Pengguna</th>
             </tr>
             <tr>
-                <td>{{ $data->user_id}}</td>
-                <td>{{ $data->username}}</td>
-                <td>{{ $data->nama}}</td>
-                <td>{{ $data->level_id}}</td>
-            </tr> 
+                <td>{{ $data }}</td>
+            </tr>
         </table>
     </body>
 </html>
