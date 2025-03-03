35647184fc9546ba144cc5fce44825044cb2d8a6
Nova Eliza Maharani <novaelzrayya@gmail.com>
2025-03-03 11:26:25 +0700

    pratikum 1 - jobsheet 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\UserModel;
+use Illuminate\Database\Capsule\Manager;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
@@ -10,9 +11,12 @@ class UserController extends Controller
 {
     public function index() {
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
-        UserModel::where('username', 'customer-1')->update($data);
+        UserModel::create($data);
            
         $user = UserModel::all();
         return view('user', ['data' => $user]); 
