1f4b6f41977dafdd3f751e45560b37df66f39841
Nova Eliza Maharani <novaelzrayya@gmail.com>
2025-03-10 12:19:24 +0700

    pratikum 2 - jobsheet 5

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
@@ -21,3 +22,5 @@ Route::get('/user/ubah/{id}', [UserController::class, 'ubah'])->name('user.ubah'
 Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan'])->name('user.ubah_simpan');
 
 Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+
+Route::get('/', [WelcomeController::class, 'index']);
